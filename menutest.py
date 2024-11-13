983e8f1c1c886eab199ab989455bf9a8d600bff2
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-11-13 12:44:13 -0800

    cleanup menu code

--- menutest.py
+++ menutest.py
@@ -20,11 +20,11 @@ tdeck = TDeck()
 display = board.DISPLAY
 
 
-def print_free_memory():
+def print_free_memory(layout):
     print(f"used mem: {gc.mem_alloc()/1024}k")
     print(f"free mem: {gc.mem_free()/1024}k")
 
-def print_platform():
+def print_platform(layout):
     print("byteorder is", sys.byteorder)
     print("impl is", sys.implementation)
     print("maxsize is", sys.maxsize)
@@ -36,70 +36,87 @@ def print_platform():
     # print("python compiler is", platform.python_compiler())
     # print("libc ver is", platform.libc_ver())
 
-def cpu_info():
+def cpu_info(layout):
     cpu = microcontroller.cpu
     print("cpu is", cpu.frequency/(1*1000*1000),"mhz", f"{cpu.temperature}C", cpu.uid, cpu.voltage)
+
+def exit_menu(lay):
+    lay.hidden = True
 
 menu = [
     ['free RAM', print_free_memory],
     ['system info', print_platform],
     ['CPU Info', cpu_info],
+    ['Exit', exit_menu]
     ]
 
 
-line_height = 12 + 5 + 5
-layout = GridLayout(
-    x=10,
-    y=10,
-    width=100,
-    height=3*line_height,
-    grid_size=(1, 3),
-    cell_padding=5,
-    divider_lines=True,
+class PopupMenu():
+    def __init__(self, items):
+        line_height = 12 + 5 + 5
+        self.items = items
+        self.selected = 0
+        self.layout = GridLayout(
+            x=10,
+            y=10,
+            width=100,
+            height=4*line_height,
+            grid_size=(1, 4),
+            cell_padding=5,
+            divider_lines=True,
+            )
+        for i in range(0,len(items)):
+            lab = label.Label(terminalio.FONT,
+                              text=items[i][0],
+                              color=0x000000,
+                              background_color=0xFFFFFF,
+                              padding_left= 5,
+                              padding_right=5,
+                              padding_top=5,
+                              padding_bottom=5,
+                              )
+            self.layout.add_content(lab, grid_position=(0, i), cell_size=(1, 1))
+        self.layout.layout_cells()
 
-)
-_labels = []
+    def perform_selected_item(self):
+        self.items[self.selected][1](self.layout)
 
-for i in range(0,len(menu)):
-    lab = label.Label(terminalio.FONT,
-                      text=menu[i][0],
-                      color=0x000000,
-                      background_color=0xFFFFFF,
-                      padding_left= 5,
-                      padding_right=5,
-                      padding_top=5,
-                      padding_bottom=5,
-                      )
-    _labels.append(lab)
-    layout.add_content(lab, grid_position=(0, i), cell_size=(1, 1))
+    def select_prev_item(self):
+        self.layout.get_cell((0,self.selected)).background_color = 0xFFFFFF
+        self.layout.get_cell((0,self.selected)).color = 0x000000
+        self.selected -= 1
+        if self.selected < 0:
+            self.selected = len(self.items) -1
+        self.layout.get_cell((0,self.selected)).background_color = 0x0000FF
+        self.layout.get_cell((0,self.selected)).color = 0xffffff
 
-layout.layout_cells()
-display.root_group = layout
+    def select_next_item(self):
+        self.layout.get_cell((0,self.selected)).background_color = 0xFFFFFF
+        self.layout.get_cell((0,self.selected)).color = 0x000000
+        self.selected += 1
+        if self.selected >= len(self.items):
+            self.selected = 0
+        self.layout.get_cell((0,self.selected)).background_color = 0x0000FF
+        self.layout.get_cell((0,self.selected)).color = 0xffffff
 
-selected = 0
+popup = PopupMenu(menu)
+display.root_group = popup.layout
+
 while True:
     keypress = tdeck.get_keypress()
     if keypress:
         print("keypress-", keypress,"-")
+        if keypress == ' ':
+            popup.layout.hidden = False
     click = tdeck.get_click()
     if click and click.pressed:
-        print("doing",menu[selected][1])
-        menu[selected][1]()
+        popup.perform_selected_item()
     for p, c in tdeck.get_trackball():
         if c > 0:
-            # print(f"{p}: {c}")
-            layout.get_cell((0,selected)).background_color = 0xFFFFFF
-            layout.get_cell((0,selected)).color = 0x000000
             if p == "right":
-                selected += 1
-                if selected >= len(_labels):
-                    selected = 0
+                popup.select_next_item()
             if p == "left":
-                selected -= 1
-                if selected < 0:
-                    selected = len(_labels) -1
-            layout.get_cell((0,selected)).background_color = 0x0000FF
-            layout.get_cell((0,selected)).color = 0xffffff
+                popup.select_prev_item()
     time.sleep(0.05)  # Simple debounce delay
 
 
