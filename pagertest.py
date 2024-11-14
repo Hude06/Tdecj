15ffcb33f141d585b81be7a932336efe5734a3e6
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-11-13 18:52:55 -0800

    refactor code

--- pagertest.py
+++ pagertest.py
@@ -1,3 +1,8 @@
+import board
+import terminalio
+import displayio
+import time
+from helper import TDeck
 
 # make some real text
 text = [
@@ -15,21 +20,21 @@ text = [
 
 # wrap into 40 char wide lines
 lines = []
-max_width = 30
+max_width = 40
 
-print("text: ",text)
+# print("text: ",text)
 line = []
 current_width = 0
 for chunk in text:
     if current_width + len(chunk[0]) > max_width:
-        print(f"SPLIT: {chunk[0]}")
+        # print(f"SPLIT: {chunk[0]}")
         words = chunk[0].split()
         before = ""
         for word in words:
             if current_width + len(before) > max_width:
-                print(f"BREAK at word '{word}'",)
+                # print(f"BREAK at word '{word}'",)
                 line.append([before,chunk[1]])
-                print("LINE:",line)
+                # print("LINE:",line)
                 lines.append(line)
                 line = []
                 before = ""
@@ -39,7 +44,7 @@ for chunk in text:
         current_width += len(before)
         continue
     if chunk[1] == 'header':
-        print("LINE:",line)
+        # print("LINE:",line)
         lines.append(line)
         lines.append([chunk])
         line = []
@@ -48,22 +53,8 @@ for chunk in text:
     line.append(chunk)
     current_width += len(chunk[0])
     # current_width += 1 # account for spaces
-print("LINE:",line)
+# print("LINE:",line)
 lines.append(line)
-
-def print_line(line):
-    out = ""
-    for sect in line:
-        if sect[1] == "header":
-            out += "\n# " + sect[0] + "\n"
-        if sect[1] == "link":
-            out += "*" + sect[0] + "*" + " "
-        if sect[1] == "bold":
-            out += "*" + sect[0] + "*" + " "
-        if sect[1] == 'plain':
-            out += sect[0] + " "
-    # print("line:",line)
-    print(out)
 
 def print_ruler():
     ten = "        "
@@ -78,15 +69,134 @@ def print_ruler():
         oo += ten2
     print(oo)
 
-print_ruler()
-print("")
+# print_ruler()
+# print("")
+# for line in lines:
+#     print(print_line(line))
+# print("")
+
+
+tdeck = TDeck()
+display = board.DISPLAY
+splash = displayio.Group()
+
+def make_spaces(txt):
+    space = ""
+    for i in range(0, len(txt)):
+        space += " "
+    return space
+
+class HighlightTerminal:
+    def __init__(self):
+        fontx, fonty = terminalio.FONT.get_bounding_box()
+        self.group = displayio.Group()
+
+        plain_palette = displayio.Palette(2)
+        plain_palette[0] = 0x000000
+        plain_palette[1] = 0x33ff33
+
+        hpal1 = displayio.Palette(2)
+        hpal1[0] = 0x000000
+        hpal1[1] = 0x0000ff
+        hpal1.make_transparent(0)
+
+        hpal2 = displayio.Palette(2)
+        hpal2[0] = 0x000000
+        hpal2[1] = 0xff0000
+        hpal2.make_transparent(0)
+
+        pgrid = displayio.TileGrid(terminalio.FONT.bitmap,
+                                        x=0,
+                                        y=0,
+                                        width=display.width // fontx,
+                                        height=display.height // fonty,
+                                        tile_width=fontx,
+                                        tile_height=fonty,
+                                        pixel_shader=plain_palette)
+        self.ptermx = terminalio.Terminal(pgrid, terminalio.FONT)
+        self.group.append(pgrid)
+
+        hgrid1 = displayio.TileGrid(terminalio.FONT.bitmap,
+                                            x=0,
+                                            y=0,
+                                            width=display.width // fontx,
+                                            height=display.height // fonty,
+                                            tile_width=fontx,
+                                            tile_height=fonty,
+                                            pixel_shader=hpal1)
+        self.hterm1 = terminalio.Terminal(hgrid1, terminalio.FONT)
+        self.group.append(hgrid1)
+
+        hgrid2 = displayio.TileGrid(terminalio.FONT.bitmap,
+                                            x=0,
+                                            y=0,
+                                            width=display.width // fontx,
+                                            height=display.height // fonty,
+                                            tile_width=fontx,
+                                            tile_height=fonty,
+                                            pixel_shader=hpal2)
+        self.hterm2 = terminalio.Terminal(hgrid2, terminalio.FONT)
+        self.group.append(hgrid2)
+
+    def print_plain(self, txt):
+        spc = make_spaces(txt)
+        print(txt, file=self.ptermx, end="")
+        print(spc, file=self.hterm1, end="")
+        print(spc, file=self.hterm2, end="")
+
+    def print_hl1(self, txt):
+        spc = make_spaces(txt)
+        print(spc, file=self.ptermx, end="")
+        print(txt, file=self.hterm1, end="")
+        print(spc, file=self.hterm2, end="")
+
+    def print_hl2(self, txt):
+        spc = make_spaces(txt)
+        print(spc, file=self.ptermx, end="")
+        print(spc, file=self.hterm1, end="")
+        print(txt, file=self.hterm2, end="")
+
+    def print_newline(self):
+        print("", file=self.ptermx, end="\r\n")
+        print("", file=self.hterm1, end="\r\n")
+        print("", file=self.hterm2, end="\r\n")
+
+    def print_line(self, line):
+        for sect in line:
+            if sect[1] == "header":
+                self.print_newline()
+                self.print_hl2(sect[0])
+                self.print_newline()
+            if sect[1] == "link":
+                self.print_hl1(sect[0])
+            if sect[1] == "bold":
+                self.print_hl1(sect[0])
+            if sect[1] == 'plain':
+                self.print_plain(sect[0])
+        self.print_newline()
+
+
+display.root_group = splash
+
+term = HighlightTerminal()
+splash.append(term.group)
+
 for line in lines:
-    print_line(line)
-print("")
+    term.print_line(line)
 
-
-
-
-
-
-
+while True:
+    keypress = tdeck.get_keypress()
+    if keypress:
+        print("keypress-", keypress,"-")
+        if keypress == ' ':
+            print("pressed space")
+    # click = tdeck.get_click()
+    # if click and click.pressed:
+        # popup.perform_selected_item()
+    # for p, c in tdeck.get_trackball():
+    #     if c > 0:
+    #         if p == "right":
+                # popup.select_next_item()
+            # if p == "left":
+                # popup.select_prev_item()
+    time.sleep(0.05)  # Simple debounce delay
