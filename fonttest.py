466aedf2d00da3ec9e2569a1381c967c5b056549
joshmarinacci <joshua@marinacci.org>
2024-11-21 19:39:10 -0800

    more work on the text grid

--- fonttest.py
+++ fonttest.py
@@ -15,59 +15,71 @@ display = setup_display()
 # - [ ] Copy a second time with a different color
 
 font = terminalio.FONT
-font_w, font_h = terminalio.FONT.get_bounding_box()
-print("font size is ", font_w, font_h)
+# print("font size is ", font_w, font_h)
 
-bitmap = displayio.Bitmap(font_w*4,font_h*3,4)
+
 palette = displayio.Palette(4)
 palette[0] = 0x000000
 palette[1] = 0xFFFFFF
 palette[2] = 0xFF0000
 palette[3] = 0x0000FF
 
-def draw_letter(bitmap, letter, font, xo, yo, color):
-    glyph = font.get_glyph(ord(letter))
-    # print("glyph is ", glyph)
-    for i in range(0, glyph.width):
-        for j in range (0, glyph.height):
-            pixel = glyph.bitmap[i + glyph.tile_index*glyph.width,j]
-            if pixel == 1:
-                pixel = color
-            bitmap[i+xo,j+yo] = pixel
+class MulticolorTextGrid:
+    def __init__(self, rows, cols, font, palette):
+        self.rows = rows
+        self.cols = cols
+        self.font = font
+        font_w, font_h = self.font.get_bounding_box()
+        print("font bitmap size", self.font.bitmap.width, self.font.bitmap.height)
+        self.font_w = font_w
+        self.font_h = font_h
+        self.palette = palette
+        self.base_text = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.bitmap = displayio.Bitmap(self.font_w*len(self.base_text),
+                                       self.font_h*len(palette),
+                                       len(palette))
+        print("new bitmap size ", self.bitmap.width, self.bitmap.height)
+        self.tile_grid = displayio.TileGrid(self.bitmap,
+                                            pixel_shader=palette,
+                                            tile_width=font_w,
+                                            tile_height=font_h,
+                                            width=font_w*10,
+                                            height=font_h*5)
 
-for n, ch in enumerate(" ABC"):
-    print("ch is ", n, ch)
-    draw_letter(bitmap, ch, terminalio.FONT, n * font_w, 0 * font_h, 1)
-    draw_letter(bitmap, ch, terminalio.FONT, n * font_w, 1 * font_h, 2)
-    draw_letter(bitmap, ch, terminalio.FONT, n * font_w, 2 * font_h, 3)
-# draw_letter(bitmap,'B',terminalio.FONT, 1*font_w,0*font_h,1)
-# draw_letter(bitmap,'C',terminalio.FONT, 2*font_w,0*font_h,1)
+        for n, ch in enumerate(self.base_text):
+            for i in range(len(palette)-1):
+                self.draw_letter(ch, n * font_w, i * font_h, i+1)
+
+    def draw_letter(self, letter, xo, yo, color):
+        # print("stamping ", letter, 'at',xo,yo,'in color',color)
+        glyph = self.font.get_glyph(ord(letter))
+        for i in range(0, glyph.width):
+            for j in range (0, glyph.height):
+                pixel = glyph.bitmap[i + glyph.tile_index * glyph.width,j]
+                if pixel == 1:
+                    pixel = color
+                self.bitmap[i+xo,j+yo] = pixel
+
+    def set_text(self, x, y, text, color):
+        for n, ch in enumerate(text):
+            fx = x + n
+            # print(ch, ord(ch),'x',x,n,fx)
+            if fx < self.cols:
+                self.tile_grid[fx,y] = ord(ch) - 65 + 1 + len(self.base_text) * color
 
 
 group = displayio.Group()
 group.x = 120
 group.y = 120
-tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette, tile_width=font_w, tile_height=font_h, width=font_w*10, height=font_h*5)
-print("ord of A is", ord('A'))
-tile_grid[0,0] = 1 # A
-tile_grid[1,0] = 3 # C
-tile_grid[2,0] = 2 # B
-tile_grid[0,1] = 1 + 4 # A in red
-tile_grid[0,2] = 1 + 4+4 # A in blue
 
-group.append(tile_grid)
+text_grid = MulticolorTextGrid(4,10,terminalio.FONT, palette)
+text_grid.set_text(0,0,"ABC",1)
+text_grid.set_text(3,0,"ABC",2)
+text_grid.set_text(6,0,"ABC",0)
+group.append(text_grid.tile_grid)
 group.scale = 2
 display.root_group = group
 while True:
-    # for x in range(0,bitmap.width):
-    #     for y in range(0, bitmap.height):
-    #         bitmap[x,y] = 0
-    #         if x % 2 == 0:
-    #             bitmap[x,y] = 1
-    #         if x % 3 == 0:
-    #             bitmap[x,y] = 2
-    # display.refresh()
-    # time.sleep(1)
     display.refresh()
     time.sleep(5)
     # print("====")
